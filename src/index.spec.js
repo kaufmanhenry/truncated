4441bd735f2efcd1c74c801651415c46a9bb1103
jakebillings <jake@jakebillings.com>
2017-07-30 14:32:02 -0600

    Add 2 more test cases (the tests fail now but demonstrate desired behavior)

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,3 +5,13 @@ test('basic array', (t) => {
   const s = truncated(['ald', 'a', 'ab', 'ac', 'aca', 'acr', 'alabama', 'bellow']).toString();
   t.is(s, 'ald,a,ab,ac,aca,acr,ala,bel');
 });
+
+test('seven days of the week', (t) => {
+  const s = truncated(['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']).toString();
+  t.is(s, 'mo,tu,we,th,fr,sa,su');
+});
+
+test('three days of the week (from README)', (t) => {
+  const s = truncated(['monday', 'tuesday', 'wednesday']).toString();
+  t.is(s, 'mo,tu,we');
+});
