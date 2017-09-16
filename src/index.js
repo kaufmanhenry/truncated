e30f5cd96a3dc1357bc26f086d876a0b57011d75
jakebillings <jake@jakebillings.com>
2017-09-16 15:13:13 -0600

    Add a couple of comments

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
  * Uses UMD module declaration. See https://github.com/umdjs/umd/blob/master/templates/returnExports.js
  * Will load as the global function "truncate" in a browser. Will return as a node module function in node. Works with AMD.
  *
+ * Currently has a pretty high algorithmic complexity. Currently looking for PRs that make the algorithm more efficient.
+ *
  * @module truncate Truncates all strings in an array to the minimum length required to keep them unique.
  */
 (function (root, factory) {
@@ -16,14 +18,19 @@
     }
 }(this, function () {
     const truncated = (array) => {
+        //Error checking/validation
+        //Basic type checking
         if (!Array.isArray(array)) throw Error('An array is required to truncate.');
         if (!array.every(element => typeof element==='string')) {
             throw Error('All elements in an array passed to truncated() must be strings.');
         }
+        //If the array already has duplicates, then we will be stuck in an infinite loop, and there's no way to
+        // complete anyway, so throw an error.
         if (hasDuplicates(array)) {
             throw Error('An array passed to truncated() must not contain duplicates at the start.');
         }
 
+        //Keep appending more characters to the mapped array until it's unique.
         let depth = 1;
         let truncatedArray;
         do {
